371b101fab378a4c63a5d4bdf050d4cc19471806
Jose Camilo Joga Guerrero <JCJG0001@ce.pucmm.edu.do>
2023-10-25 18:40:58 -0400

    removed credentials

--- test.py
+++ test.py
@@ -13,10 +13,10 @@ with SB(uc=True) as driver:
     driver.get(
         "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=https%3A%2F%2Fdevelopers.google.com%2Foauthplayground&prompt=consent&response_type=code&client_id=407408718192.apps.googleusercontent.com&scope=email&access_type=offline&flowName=GeneralOAuthFlow"
     )
-    driver.type("#identifierId", "emelycamilo1727@gmail.com")
+    driver.type("#identifierId", EMAIL)
     driver.click("#identifierNext > div > button")
 
-    driver.type("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input", "Teamo1727")
+    driver.type("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input", PASSWORD)
     driver.click("#passwordNext > div > button")
 
     sleep(5)
